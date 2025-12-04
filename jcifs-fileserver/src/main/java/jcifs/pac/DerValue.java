5d70d0b14ce8627c979db5d45e890f0cb9813e35
alignedfibers <starsnic@gmail.com>
2025-12-04 13:08:45 -0600

    Working through understanding and porting over Oid and DerValue from IETF sun.security.util to use BouncyCastle ASN1 as is already used elsewhere in jfileserver_sa heavily a jcifs_jlan variant - this will allow portability to run more functionality from jfileserver on non-standard non-oracle jvm - Steps completed in the port thus far are needed classes from GSS, Sun Security, OpenJDK using open alternatives for the same functionality

--- jcifs-fileserver/src/main/java/jcifs/pac/DerValue.java
+++ jcifs-fileserver/src/main/java/jcifs/pac/DerValue.java
@@ -17,14 +17,66 @@ import org.bouncycastle.asn1.ASN1Primitive;
  */
 public class DerValue {
     private ASN1Primitive value;
+    /***
+     * Using this parse method for now as kind of a place holder in case
+     * we at some point need to add functionality here.
+     * **/
+    private ASN1Primitive parse(InputStream in) throws IOException {
+        try (ASN1InputStream asn = new ASN1InputStream(in)) {
+            ASN1Primitive obj = asn.readObject();
 
-    public DerValue(InputStream in) throws IOException {
-        ASN1InputStream asn = new ASN1InputStream(in);
-        this.value = asn.readObject();
-        if (this.value == null) {
-            throw new IOException("DER: no data in InputStream");
+            if (obj == null) {
+                throw new IOException("DER: no data in InputStream");
+            }
+
+            return obj;
         }
     }
+
+
+
+    /**
+     * Get an ASN1/DER encoded datum from an input stream.  The
+     * stream may have additional data following the encoded datum.
+     * In case of indefinite length encoded datum, the input stream
+     * must hold only one datum.
+     *
+     * @param in the input stream holding a single DER datum,
+     *  which may be followed by additional data
+     */
+    public DerValue(InputStream in) throws IOException {
+        this.value = parse(in);
+    }
+
+    /**
+     * Get an ASN1/DER encoded datum from an input stream w/ additional
+     * arg to control whether DER checks are enforced.
+     */
+    DerValue(InputStream in, boolean allowBER) throws IOException {
+        // allowBER ignored ASN1 safely handles internally, kept for any compat issue
+        this.value = parse(in);
+    }
+
+    /**
+     * Get an ASN.1/DER encoded datum from a buffer w/ additional
+     * arg to control whether DER checks are enforced.
+     */
+    DerValue(byte[] buf, boolean allowBER) throws IOException {
+        this(new ByteArrayInputStream(buf), allowBER);   // #2
+    }
+
+    /**
+     * Get an ASN.1/DER encoded datum from a buffer.  The
+     * entire buffer must hold exactly one datum, including
+     * its tag and length.
+     *
+     * @param buf buffer holding a single DER-encoded datum.
+     */
+    public DerValue(byte[] buf) throws IOException {
+        this(buf, true);
+    }
+
+
 
     public byte[] toByteArray() throws IOException {
         return value.getEncoded();  // DER-encode the parsed value
@@ -37,17 +89,9 @@ public class DerValue {
         return (ASN1ObjectIdentifier) value;
     }
 
-    public DerValue(byte[] buf) throws IOException {
-        this(buf, true);                      // #1
-    }
 
-    DerValue(byte[] buf, boolean allowBER) throws IOException {
-        this(new ByteArrayInputStream(buf), allowBER);   // #2
-    }
 
-    DerValue(InputStream in, boolean allowBER) throws IOException {
-        data = init(false, in, allowBER);     // #3 (the real parsing)
-    }
+
 
 
     //public DerValue(byte[] buf, boolean allowBER){}
